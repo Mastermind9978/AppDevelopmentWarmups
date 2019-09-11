dc1515329759ed4e346533de46cf0a0073b695bd
Mastermind9978 <48933660+Mastermind9978@users.noreply.github.com>
2019-09-11 14:21:08 -0400

    First Commit

---
+++ KeyBoard.java
@@ -0,0 +1,33 @@
+import java.awt.event.*;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class Keyboard extends JPanel
+{
+	public Keyboard()
+	{
+		JFrame frame = new JFrame();
+		frame.add(this);
+		frame.setVisible(true);
+		setFocusable(true);
+
+		addKeyListener( new KeyListener(){
+
+			public void keyPressed(KeyEvent e)
+			{
+				System.out.println(e.getKeyChar());
+			}
+			public void keyReleased(KeyEvent e)
+			{
+
+			}
+			public void keyTyped(KeyEvent e){}
+
+		});
+	}
+
+	public static void main(String[]args){
+		Keyboard demo = new Keyboard();
+	}
+
+}
